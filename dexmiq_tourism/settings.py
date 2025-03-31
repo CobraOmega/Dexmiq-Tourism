d5defa4167d9f1448ab376844279e110e240da22
CobraOmega <yashad04@gmail.com>
2025-03-31 15:21:52 +0530

    Fixed hero section issue

--- dexmiq_tourism/settings.py
+++ dexmiq_tourism/settings.py
@@ -138,22 +138,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_URL = 'static/'
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# For dynamic image storage
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# For static files
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Custom model for authentication
 AUTH_USER_MODEL = 'customer.CustomUser'
--- dexmiq_tourism/urls.py
+++ dexmiq_tourism/urls.py
@@ -16,15 +16,17 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings
 from django.conf.urls.static import static
-from . import settings 
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html"), name='homepage'),
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")), #For API links/paths
-    path('payment/', include('payment.urls'))
+    path('payment/', include('payment.urls')),
 ]
+
 if settings.DEBUG:  
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
